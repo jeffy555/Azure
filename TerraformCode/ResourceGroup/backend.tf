e62cc0e572869ae5c88097278152605f46ca4237
jeffy555 <jeffersonimmanuel5@gmail.com>
2022-10-11 17:23:55 +0530

    modify

--- TerraformCode/ResourceGroup/backend.tf
+++ TerraformCode/ResourceGroup/backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "azurerm" {
-    storage_account_name = "terraformstatefordemo05"
-    container_name       = "tfstate"
-    key                  = "demo.terraform.tfstate"
-    subscription_id      = "__subid__"
-    tenant_id            = "__tenantid__"
-  }
-}
+# terraform {
+#   backend "azurerm" {
+#     storage_account_name = "terraformstatefordemo05"
+#     container_name       = "tfstate"
+#     key                  = "demo.terraform.tfstate"
+#     subscription_id      = "__subid__"
+#     tenant_id            = "__tenantid__"
+#   }
+# }
