bc36b159303b4cf43b63b3f0835a07ebefdf5c17
jeffy555 <jeffersonimmanuel5@gmail.com>
2022-10-11 08:53:07 +0530

    Added keyvault details to Backend config

--- TerraformCode/ResourceGroup/backend.tf
+++ TerraformCode/ResourceGroup/backend.tf
@@ -3,5 +3,7 @@ terraform {
     storage_account_name = "terraformstatefordemo05"
     container_name       = "tfstate"
     key                  = "demo.terraform.tfstate"
+    subscription_id      = "__SUB_ID__"
+    tenant_id            = "__Tenant_ID__"
   }
 }
--- TerraformCode/StorageBackend/main.tf
+++ TerraformCode/StorageBackend/main.tf
@@ -17,3 +17,32 @@ resource "azurerm_storage_container" "Container" {
   storage_account_name  = azurerm_storage_account.StorageAccount.name
   container_access_type = "container"
 }
+
+resource "azurerm_key_vault" "example" {
+  name                        = "Secretstoragedemo05"
+  location                    = "East US"
+  resource_group_name         = "StorageBackend"
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
+}
